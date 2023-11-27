d52002ec3d5ae5b3770f8de812b6b032781cf0a5
BasurajPoojari <25427194-BasurajPoojari@users.noreply.replit.com>
2023-11-27 21:44:52 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+// 1) Given an string reverse each word in the sentances 
+
+// let arr = 'basuraj How are you'
+// let savedstr = arr.split(' ').map(function(word) {
+//   return word.split('').reverse().join('')
+// })
+
+// console.log(savedstr.join(' '))
+
+
+// Q2 how to check  if an object is an array or object 
+
+// function checkArray(elem) {
+//   return Array.isArray(elem)
+// }
+
+// console.log(checkArray([]))
+// console.log(checkArray({}))
+
+// how to empty an array in java script 
+// * do not reset it to new array and dont loop throughto pop each method 
+
+// let arr = [1,3,5,6,7,]
+// arr.length =0
+// console.log(arr.length)
+
+// Q4 how to check if num is na interger 
+// let a = 12.5
+
+// if (a%1==0){
+//   console.log( `${a} is an interger `)
+// }else{
+//   console.log( `${a} is not an integer `)
+// }
+
+
+// Q5 make this works means duplicate an array 
+// example arr=  [1,2,3] output should be [1,2,3,1,2,3]
+// let arr = [1,2,4]
+// function dublicate (arr){
+//    return arr.concat(arr)
+// }
+
+// console.log(dublicate(arr))
